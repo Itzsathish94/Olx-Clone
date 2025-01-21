52d4013424bd901ec5b0f58a7eccb3704507de10
Sathish <rhbsk1994@gmail.com>
2025-01-21 20:05:24 +0530

    login added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,24 @@ import Logo from '../assets/olx-logo.png'
 import Lens from '../assets/search_icon.png' 
 import down_icon from '../assets/down_icon2.png'
 import search_field_icon from '../assets/search_field_icon.png'
-
+import Login from './Login'
+import { useState } from 'react'
 
 
 const Navbar = () => {
+
+    const [loginPop,setLogInPop] = useState(false);
+
   return (
+    <>
     <div className='flex p-4' >
       <img className='w-9 h-9' src={Logo} alt="Olx logo" />
-      <div className='flex border border-spacing-1 w-64 p-2 border-black ml-5'>
+      <div className='flex border-2 border-spacing-1 w-64 p-2 border-black ml-5'>
         <img className='w-3 h-3 mt-1' src={Lens} alt="search lens icon" />
         <input placeholder='Location' className='ml-3'/>
         <img className="w-4 h-4 mt-1" src={down_icon} alt="down arrow icon" />
       </div>
-      <div className='flex h-12 ml-4 border border-black'>
+      <div className='flex h-12 ml-4 border-2 border-black'>
         <input className='ml-3 w-[720px]' placeholder="Find Cars, Mobile phones and more" type="text" />
         <img src={search_field_icon} alt="search field icon" />
       </div>
@@ -22,13 +27,15 @@ const Navbar = () => {
         <h1 className='font-semibold' >ENGLISH</h1>
         <img className="w-5 h-5 mt-0.5 ml-2" src={down_icon} alt="down arrow icon" />
       </div>
-      <div className='flex h-12 p-3 ml-6 cursor-pointer underline hover:no-underline'>
+      <div onClick={()=>setLogInPop(!loginPop)} className='flex h-12 p-3 ml-6 cursor-pointer underline hover:no-underline'>
         <h1 className='font-bold text-lg' >Login</h1>
       </div>
       <div className='w-28 flex h-12 p-2 ml-6 cursor-pointer underline hover:no-underline rounded-full border border-yellow-500'>
         <h1 className='font-bold text-lg ml-3' >+ SELL</h1>
       </div>
     </div>
+    {loginPop && <Login setLoginPop={setLogInPop}/>}
+    </>
   )
 }
 
---
+++ src/firebase/setup.jsx
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDY604aHpNxSPdasZVN0OE0Mas0ZpKFO3A",
+  authDomain: "olx-clone-react-e8fbd.firebaseapp.com",
+  projectId: "olx-clone-react-e8fbd",
+  storageBucket: "olx-clone-react-e8fbd.firebasestorage.app",
+  messagingSenderId: "378812764862",
+  appId: "1:378812764862:web:d6fd23594d02795d2744ef"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
